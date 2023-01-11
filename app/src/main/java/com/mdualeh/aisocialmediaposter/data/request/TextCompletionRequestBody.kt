65874a62ccfa8451fdebe0cea370709ece1f084e
Mustafa Dualeh <mustafa.mtd@gmail.com>
2023-01-11 22:41:29 +0900

    Fixed issue with post request

--- app/src/main/java/com/mdualeh/aisocialmediaposter/data/request/TextCompletionRequestBody.kt
+++ app/src/main/java/com/mdualeh/aisocialmediaposter/data/request/TextCompletionRequestBody.kt
@@ -1,8 +1,11 @@
 package com.mdualeh.aisocialmediaposter.data.request
 
+import com.squareup.moshi.Json
+
 data class TextCompletionRequestBody(
-    val maxTokens: Int,
-    val temperature: Double = 0.9,
     val model: String = "text-davinci-003",
     val prompt: String,
+    @field:Json(name = "max_tokens")
+    val maxTokens: Int,
+    val temperature: Double = 0.9,
 )
