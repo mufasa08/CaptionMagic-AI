74dd9baacb8dc864305fef9c9a1de0b8104869b3
Mustafa Dualeh <mustafa.mtd@gmail.com>
2023-01-16 19:51:36 +0900

    Removed compose preview causing release build issue

--- app/src/main/java/com/devinjapan/aisocialmediaposter/ui/components/ImagePicker.kt
+++ app/src/main/java/com/devinjapan/aisocialmediaposter/ui/components/ImagePicker.kt
@@ -12,13 +12,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.devinjapan.aisocialmediaposter.R
 import com.devinjapan.aisocialmediaposter.ui.viewmodels.CaptionGeneratorViewModel
 
-@Preview
 @Composable
 fun ImagePicker(
     modifier: Modifier,
