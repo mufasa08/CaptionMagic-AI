029a45ebc071ba6d64df990d406bf1d28dc3b9f3
Mustafa Dualeh <mustafa.mtd@gmail.com>
2023-01-23 19:31:37 +0900

    Updated string

--- app/src/main/java/com/devinjapan/aisocialmediaposter/data/mappers/OpenAIResponseMapper.kt
+++ app/src/main/java/com/devinjapan/aisocialmediaposter/data/mappers/OpenAIResponseMapper.kt
@@ -6,7 +6,7 @@ import com.devinjapan.aisocialmediaposter.domain.model.TextCompletion
 fun TextCompletionDto.toTextCompletion(hideHashTags: Boolean): TextCompletion {
     return TextCompletion(
         choices = this.choices?.map {
-            val text = it.text + if (!hideHashTags) " #ChatGPT #captionmagic #AI" else ""
+            val text = it.text + if (!hideHashTags) " #ChatGPT #CaptionMagic #AI" else ""
             TextCompletion.ChoicesItem(it.index, text)
         }
     )
