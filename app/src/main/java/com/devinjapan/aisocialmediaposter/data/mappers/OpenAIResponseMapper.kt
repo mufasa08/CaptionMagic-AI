17d85165d927b7abb0aeda93913eb2dc10c6d074
Mustafa Dualeh <mustafa.mtd@gmail.com>
2023-01-23 19:26:38 +0900

    Changed extra hashtags

--- app/src/main/java/com/devinjapan/aisocialmediaposter/data/mappers/OpenAIResponseMapper.kt
+++ app/src/main/java/com/devinjapan/aisocialmediaposter/data/mappers/OpenAIResponseMapper.kt
@@ -6,7 +6,7 @@ import com.devinjapan.aisocialmediaposter.domain.model.TextCompletion
 fun TextCompletionDto.toTextCompletion(hideHashTags: Boolean): TextCompletion {
     return TextCompletion(
         choices = this.choices?.map {
-            val text = it.text + if (!hideHashTags) " #madewithAI #ChatGPT" else ""
+            val text = it.text + if (!hideHashTags) " #ChatGPT #captionmagic #AI" else ""
             TextCompletion.ChoicesItem(it.index, text)
         }
     )
