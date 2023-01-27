376833a884b3fd18932a7db0fefa78c2e4d4be05
Mustafa Dualeh <mustafa.mtd@gmail.com>
2023-01-27 16:12:34 +0900

    Changed gradle to kotlin gradle script from groovy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,21 @@
 buildscript {
-
-    ext {
-        compose_version('1.3.3')
-    }
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
         classpath("com.google.gms:google-services:4.3.14")
-        classpath('com.google.firebase:firebase-crashlytics-gradle:2.9.2')
-        classpath('com.google.android.gms:oss-licenses-plugin:0.10.6')
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
     }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+} // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id('com.android.application') version '7.2.2' apply false
-    id('com.android.library') version '7.2.2' apply false
-    id('org.jetbrains.kotlin.android') version '1.7.20' apply false
+    id("com.android.application") version "7.2.2" apply false
+    id("com.android.library") version "7.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
     id("io.gitlab.arturbosch.detekt") version ("1.22.0")
 
     kotlin("multiplatform").version("1.7.10").apply(false)
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
